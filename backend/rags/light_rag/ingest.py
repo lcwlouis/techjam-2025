2f60bedb65a9049b0fe58450f6bc09555a418e59
Hrishikesh Sathyian <e1121418@u.nus.edu>
2025-08-27 17:38:52 +0800

    Add more documentation for lightRAG

--- backend/rags/light_rag/ingest.py
+++ backend/rags/light_rag/ingest.py
@@ -1,15 +1,23 @@
 from typing import Optional
+
+import tenacity
 from .utils import init_light_rag, close_light_rag
 import openai
+
+## This is just a simple naive ingest function for now, to integrate Louis' ingestion logic
 async def ingest(text: str, region_code:str):
     """Ingest new text into a region workspace."""
     try:
         rag = await init_light_rag(region_code=region_code)
         await rag.ainsert(input=text)
-    except openai.RateLimitError as e:
-        # Handle insufficient quota error
-        print(f"OpenAI API quota exceeded: {e}")
-        # Optionally, re-raise or handle as needed
+
+    except tenacity.RetryError as e:
+        inner = e.last_attempt.exception()
+        return {"status": "error", "reason": "retry_failed during ingestion, likely API error", "detail": str(inner)}
+
+    except Exception as e:
+        return {"status": "error","reason": "unexpected error during data ingestion", "detail": str(e)}
+
     finally:
         await close_light_rag(rag)
 
